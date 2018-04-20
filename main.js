31c27b2e516f1029088bb6da66b93b20eab4ecc5
yyhsz <591985864@qq.com>
2018-04-20 21:08:14 +0800

    add clear,save

--- main.js
+++ main.js
@@ -1,12 +1,14 @@
 var canvas = document.getElementById('canvas'); var ctx = canvas.getContext('2d'); var usingpencil = false;
 var newPoint = { x: undefined, y: undefined }; lastPoint = { x: undefined, y: undefined }; var usingrubber = false;
-var rubberEnabled = false; var pencilEnabled = false;
+var rubberEnabled = false; var pencilEnabled = false;var link = document.getElementById('save');
 
 //自动设置宽高
 setPageSize()
 //监听用户操作
 listenToUser()
-
+//
+ctx.fillStyle = "#fff";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
 
 
 
@@ -26,8 +28,8 @@ function listenToUser() {
                 ctx.clearRect(x - 10, y - 10, 20, 20); usingrubber = true;
             }
             if (pencilEnabled) {
-                usingpencil = true; drawCircle(x, y, 2)
                 lastPoint.x = x; lastPoint.y = y;
+                usingpencil = true; drawCircle(x, y, 2)
             }
         }
 
@@ -91,14 +93,55 @@ function listenToUser() {
 //点击橡皮擦
 rubber.onclick = function (a) {
     pencilEnabled = false; rubberEnabled = true;
-    rubber.classList.add('active');
-    pencil.classList.remove('active');
+    rubber.classList.add('active'); pencil.classList.remove('active');
+    blue.classList.remove('active'); yellow.classList.remove('active'); red.classList.remove('active');
 }
 //点击铅笔
 pencil.onclick = function (a) {
-    pencilEnabled = true; rubberEnabled = false;
-    pencil.classList.add('active');
-    rubber.classList.remove('active');
+    pencil.classList.add('active');rubber.classList.remove('active'); ctx.lineWidth = 4; 
+    pencilEnabled = true; rubberEnabled = false;ctx.strokeStyle = 'black';
+    blue.classList.remove('active'); yellow.classList.remove('active'); red.classList.remove('active');
+}
+//改变铅笔颜色
+
+red.onclick = function () {
+    if (pencilEnabled == true) {
+
+        ctx.strokeStyle = 'red'; red.classList.add('active');ctx.fillStyle = 'red';
+        blue.classList.remove('active'); yellow.classList.remove('active');
+    }
+}
+blue.onclick = function () {
+    if (pencilEnabled == true) {
+        ctx.strokeStyle = 'blue'; blue.classList.add('active');ctx.fillStyle = 'blue';
+        yellow.classList.remove('active'); red.classList.remove('active');
+    }
+}
+yellow.onclick = function () {
+    if (pencilEnabled == true) {
+    ctx.strokeStyle = 'yellow'; yellow.classList.add('active');ctx.fillStyle = 'yellow';
+        blue.classList.remove('active'); red.classList.remove('active');
+    }
+}
+//改变铅笔粗细
+thin.onclick = function(){
+    ctx.lineWidth = 6; 
+}
+thick.onclick = function(){
+    ctx.lineWidth = 8; 
+}
+thicker.onclick = function(){
+    ctx.lineWidth = 10; 
+}
+
+//清空画板
+clear.onclick = function(){
+    ctx.clearRect(0,0,canvas.width,canvas.height)
+}
+//保存作画
+save.onclick = function(){
+    save.href = canvas.toDataURL();save.download = 'xxx';
+    save.click();
     
 }
 
@@ -108,7 +151,7 @@ function drawCircle(x, y, radius) {
     ctx.beginPath(); ctx.arc(x, y, radius, 0, Math.PI * 2); ctx.fill();
 }
 function drawLine(x1, y1, x2, y2) {
-    ctx.beginPath(); ctx.moveTo(x1, y1); ctx.lineWidth = 4; ctx.lineTo(x2, y2); ctx.stroke();
+    ctx.beginPath(); ctx.moveTo(x1, y1);ctx.lineTo(x2, y2); ctx.stroke();
 }
 
 
@@ -119,7 +162,7 @@ function setPageSize() {
         pageSize()
     }
 }
-function pageSize() {  
+function pageSize() {
     //设置页面大小函数
     var pageHeight = document.documentElement.clientHeight; var pageWidth = document.documentElement.clientWidth;
     canvas.height = pageHeight; canvas.width = pageWidth;
